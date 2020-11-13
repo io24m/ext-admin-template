8bd2103e9cdf4cb30d5dcec58880eac6a1bb72a7
io24m <io24m@outlook.com>
2020-11-13 17:22:28 +0800

    i

--- app/common/login/LoginController.js
+++ app/common/login/LoginController.js
@@ -2,17 +2,10 @@ Ext.define("Admin.common.login.LoginController", {
     extend: 'Ext.app.ViewController',
     alias: 'controller.common_login',
     onLogin: function () {
-        // This would be the ideal location to verify the user's credentials via
-        // a server-side lookup. We'll just move forward for the sake of this example.
-
-        // Set the localStorage value to true
+        //进行用户验证
+        //获取权限数据
         localStorage.setItem("isLogin", true);
-
-        // Remove Login Window
         this.getView().destroy();
-
-        // Add the main view to the viewport
         Ext.widget('common_layout_main');
-        // this.redirectTo('dashboard', true);
     }
 });
