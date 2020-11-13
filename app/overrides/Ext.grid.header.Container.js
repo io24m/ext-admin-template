a32cda927f5230a85a228bb0c5b07ddd1af42811
io24m <io24m@outlook.com>
2020-11-13 20:18:03 +0800

    i

---
+++ app/overrides/Ext.grid.header.Container.js
@@ -0,0 +1,7 @@
+Ext.define('Admin.override.grid.header.Container', {
+    override: 'Ext.grid.header.Container',
+    sortAscText: '升序',
+    sortDescText: '降序',
+    sortClearText: '清理排序',
+    columnsText: '列',
+});
