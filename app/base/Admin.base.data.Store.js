1c1309bef89553670319eeb35615e693e84048ee
io24m <io24m@outlook.com>
2020-11-15 11:03:11 +0800

    i

--- app/base/Admin.base.data.Store.js
+++ app/base/Admin.base.data.Store.js
@@ -1,20 +1,7 @@
 Ext.define('Admin.base.data.Store', {
     extend: 'Ext.data.Store',
     proxy: {
-        type: 'ajax',
-        actionMethods: {
-            create: 'POST',
-            read: 'GET',
-            update: 'POST',
-            destroy: 'POST'
-        },
-        reader: {
-            type: 'json',
-            rootProperty: 'result.songs',
-            totalProperty: "result.songCount"
-        },
-        pageParam: 'offset',
-        limitParam: 'limit',
+        type: 'api',
         api: {
             // read: '',
         }
---
+++ app/proxy/Api.js
@@ -0,0 +1,17 @@
+Ext.define('Admin.proxy.Api', {
+    extend: 'Ext.data.proxy.Ajax',
+    alias: 'proxy.api',
+    actionMethods: {
+        create: 'POST',
+        read: 'GET',
+        update: 'POST',
+        destroy: 'POST'
+    },
+    reader: {
+        type: 'json',
+        rootProperty: 'result.songs',
+        totalProperty: "result.songCount"
+    },
+    pageParam: 'offset',
+    limitParam: 'limit',
+});
