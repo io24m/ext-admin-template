377c1ccc75abf0c8cec6b3103fba6f438df55890
io24m <io24m@outlook.com>
2020-11-19 11:24:26 +0800

    i

--- app/AppInit.js
+++ app/AppInit.js
@@ -1,7 +1,45 @@
 Ext.define('App.AppInit', {
     singleton: true,
-    init: function () {
+    loadJSCSSFile: function (fileName, callback) {
+        var fileref = null;
+        var arr = fileName.split('.');
+        switch (arr[arr.length - 1]) {
+            case 'js':
+                fileref = document.createElement('script');
+                fileref.setAttribute('type', 'text/javascript');
+                fileref.setAttribute('src', fileName);
+                break;
+            case 'css':
+                fileref = document.createElement('link');
+                fileref.setAttribute('rel', 'stylesheet');
+                fileref.setAttribute('type', 'text/css');
+                fileref.setAttribute('herf', fileName);
+                break;
+        }
+        if (fileref != null) {
+            if (fileref.readyState) { // IE  
+                fileref.onreadystatechange = function () {
+                    if (fileref.readyState == 'loaded' || fileref.readyState == 'complete') {
+                        fileref.onreadystatechange = null;
+                        callback();
+                    }
+                };
+            } else { // Others: Firefox, Safari, Chrome, and Opera  
+                fileref.onload = function () {
+                    callback();
+                };
+            }
+            document.getElementsByTagName('head')[0].appendChild(fileref);
+        }
+    },
+    init: function () { 
+        // App.common.utils.Shared.addCss('resources/lib/nprogress-0.2.0/nprogress.css')
+        // App.common.utils.Shared.addJs('resources/lib/nprogress-0.2.0/nprogress.js')
+
+
         var me = this;
+        // me.loadJSCSSFile('resources/lib/nprogress-0.2.0/nprogress.css',function(){});
+        // me.loadJSCSSFile('resources/lib/nprogress-0.2.0/nprogress.js',function(){});
         Ext.Date.monthNames = [
             '一月', '二月', '三月',
             '四月', '五月', '六月',
