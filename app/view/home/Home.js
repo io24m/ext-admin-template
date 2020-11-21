edd601fc75b6cbefb48be5c0f8da4d5c76a67d97
io24m <io24m@outlook.com>
2020-11-21 11:23:46 +0800

    i

--- app/view/home/Home.js
+++ app/view/home/Home.js
@@ -1,17 +1,43 @@
 Ext.define('App.view.home.Home', {
     extend: 'Ext.container.Container',
     xtype: 'home',
-    margin: '10 0 0 10', 
+    margin: '10 0 0 10',
     scrollable: 'y',
+    defaults: {
+        margin: '2 2 2 2',
+    },
     items: [{
         xtype: 'button',
-        text: 'click',
+        text: 'info',
         handler: function () {
             App.util.Http.post({
-                url: '/err'
+                url: '/auth'
             }).then(function (res) {
-                debugger
+                App.util.Toast.info("信息")
             })
+        }
+    }, {
+        xtype: 'button',
+        text: 'notify',
+        handler: function () {
+            App.util.Toast.notify("通知")
+        }
+    }, {
+        xtype: 'button',
+        text: 'warn',
+        handler: function () {
+            App.util.Toast.warn("警告")
+        }
+    }, {
+        xtype: 'button',
+        text: 'error',
+        handler: function () {
+            App.util.Toast.error("错误")
+            // App.util.Http.post({
+            //     url: '/err'
+            // }).then(function (res) {
+            //     debugger
+            // })
         }
     }]
 });
